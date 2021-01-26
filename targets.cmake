c6a3d682b0f035a387e9df51d6d58d1a0cf000b0
quandangv <quandangv@gmail.com>
2021-01-27 02:47:40 +0700

    Copy header files from their directories to a single dir to be included in the physical library

--- targets.cmake
+++ targets.cmake
@@ -11,10 +11,15 @@
 
 # Add the build result of the shared library to be used by the executable and external tests {{{
   add_library(${PROJECT_NAME}_physical SHARED IMPORTED)
-  target_include_directories(${PROJECT_NAME}_physical INTERFACE ${PUBLIC_HEADERS_DIR})
+  target_include_directories(${PROJECT_NAME}_physical INTERFACE ${CMAKE_BINARY_DIR}/public-headers)
   add_dependencies(${PROJECT_NAME}_physical ${PROJECT_NAME})
   set_property(TARGET ${PROJECT_NAME}_physical PROPERTY
                IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/bin/lib${PROJECT_NAME}.so)
+  # Copy the public header files to a different directory to be included to the physical library
+  foreach(header ${PUBLIC_HEADERS})
+    get_filename_component(header_name ${header} NAME)
+    configure_file(${header} ${CMAKE_BINARY_DIR}/public-headers/${header_name} COPYONLY)
+  endforeach()
 # }}}
 
 ## Add executable to provide a command-line interface
