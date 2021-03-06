77316568b23f526cef09ebf5073e1153ea0ceb43
quandangv <quandangv@gmail.com>
2021-03-06 23:53:02 +0700

    Rename built library and header files directory

--- targets.cmake
+++ targets.cmake
@@ -8,7 +8,7 @@ function(add_shared_lib name sources include_dirs public_headers header_install_
     PUBLIC_HEADER DESTINATION include/${header_install_path})
 
   # Put all header files in one place to be used by external tests
-  set(assembly_path ${CMAKE_BINARY_DIR}/public-headers/${header_install_path})
+  set(assembly_path ${CMAKE_BINARY_DIR}/include/${header_install_path})
   file(MAKE_DIRECTORY ${assembly_path})
   foreach(header ${public_headers})
     get_filename_component(header_name ${header} NAME)
@@ -21,7 +21,7 @@ function(add_shared_lib name sources include_dirs public_headers header_install_
   endif()
 
   set_target_properties(${name} PROPERTIES
-      VERSION ${PROJECT_VERSION} LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin")
+      VERSION ${PROJECT_VERSION} LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib")
 endfunction()
 
 ## Add executable to provide a command-line interface
